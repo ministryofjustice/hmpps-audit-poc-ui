7f0e0e2573217ef261ad9eccc487435b141b5953
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2023-10-29 19:12:55 +0000

    auditService.ts

---
+++ server/services/auditService.ts
@@ -0,0 +1,54 @@
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs'
+import logger from '../../logger'
+import config from '../config'
+
+class AuditService {
+  private sqsClient: SQSClient
+
+  constructor(private readonly queueUrl = config.apis.audit.queueUrl) {
+    this.sqsClient = new SQSClient({
+      region: config.apis.audit.region,
+    })
+  }
+
+  async sendAuditMessage({
+    action,
+    who,
+    subjectId,
+    subjectType,
+    details,
+  }: {
+    action: string
+    who: string
+    subjectId?: string
+    subjectType?: string
+    details?: string
+  }) {
+    try {
+      const message = JSON.stringify({
+        what: action,
+        when: new Date(),
+        who,
+        subjectId,
+        subjectType,
+        service: config.apis.audit.serviceName,
+        details,
+      })
+
+      const messageResponse = await this.sqsClient.send(
+        new SendMessageCommand({
+          MessageBody: message,
+          QueueUrl: this.queueUrl,
+        }),
+      )
+
+      logger.info(`SQS message sent (${messageResponse.MessageId})`)
+      return message
+    } catch (error) {
+      logger.error('Problem sending message to SQS queue', error)
+      throw error
+    }
+  }
+}
+
+export const auditService = new AuditService()
